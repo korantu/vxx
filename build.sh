9b32bf51eae43103779f8bcae7d1b0bcd08deec7
Konstantin Levinski <konstantin.levinski@gmail.com>
2009-05-28 14:09:53 +0800

    Build process fix - proper makefile selection and Linux platform

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 echo Using `uname -o`...
-make -f Makefile.platform.`uname -o` $@
+case `uname -o` in
+Cygwin ) make -f Makefile.platform.Cygwin $@ ;;
+*Linux ) make -f Makefile.platform.Linux $@ ;;
+* ) echo "No custom makefile for the platform";;
+esac
